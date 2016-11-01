3ffec48331ed2362c8594ba840f5f116588eaca2
Tyler Sharp <tyler.v.sharp@gmail.com>
2016-11-01 12:17:41 -0400

    Edit profile page, add additional stats, add methods to User class.

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,12 +2,12 @@ class Game < ApplicationRecord
   belongs_to :user
 
   validates :board, :user_id, :outcome, presence: true
-  validates :outcome, numericality: { only_integer: true, greater_than: -2, less_than: 2  }
+  validates :outcome, numericality: { only_integer: true, greater_than: -2, less_than: 3  }
 
   def game_status
     self.outcome == -1 ? "Loss" :
     self.outcome == 0 ? "Tie" :
-    self.outcome == 1 ? "Win" : "Not Found"
+    self.outcome == 1 ? "Win" : "Pending"
   end
 
   def game_time
