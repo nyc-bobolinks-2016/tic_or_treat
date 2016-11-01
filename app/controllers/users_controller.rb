f31cdeacb1dcfff3db0bbb8098ebbc674fa4e766
Wellington Vicioso <wellington.vicioso@gmail.com>
2016-10-31 20:37:47 -0400

    add profile page and style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find_by(id: params[:id])
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 12}
 
+  def games_won
+    self.games.select {|game| game.outcome == 1 }
+  end
+
+  def games_tied
+    self.games.select {|game| game.outcome == 0 }
+  end
+
+  def games_lost
+    self.games.select {|game| game.outcome == -1 }
+  end
 end
